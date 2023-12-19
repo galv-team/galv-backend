363f8a94c3cf5afd31ce054c1ca431fbce7f5e3d
Matt Jaquiery <matt.jaquiery@dtc.ox.ac.uk>
2023-12-19 16:15:25 +0000

    docs config

--- docs/conf.py
+++ docs/conf.py
@@ -2,6 +2,8 @@
 #
 # For the full list of built-in configuration values, see the documentation:
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
+import json
+import os
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -24,5 +26,20 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
+
+# -- Script from codingwiththomas.com -------------------------------------------------
+# https://www.codingwiththomas.com/blog/my-sphinx-best-practice-for-a-multiversion-documentation-in-different-languages
+
+# get the environment variable build_all_docs and pages_root
+build_all_docs = os.environ.get("build_all_docs")
+pages_root = os.environ.get("pages_root", "")
+
+# if not there, we dont call this
+if build_all_docs is not None:
+  # we set the html_context with current version and versions from json file
+  html_context = {
+    'current_version' : os.environ.get("current_version", "no-curent-version-envvar"),
+    'versions' : json.load(open("tags.json", "r")),
+  }
