5e6478a94895fd6992c9b7e1433cc3815cc9b78d
Matt Jaquiery <matt.jaquiery@dtc.ox.ac.uk>
2024-03-19 14:35:51 +0000

    update version

--- backend_django/config/settings_base.py
+++ backend_django/config/settings_base.py
@@ -20,7 +20,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 import os
 
-API_VERSION = "2.1.20"
+API_VERSION = "2.1.21"
 
 try:
     USER_ACTIVATION_TOKEN_EXPIRY_S = int(os.environ.get("DJANGO_USER_ACTIVATION_TOKEN_EXPIRY_S"))
