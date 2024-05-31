14fdd75a354c1c7f8454c032e9e69f3ebac2787f
Matt Jaquiery <matt.jaquiery@dtc.ox.ac.uk>
2024-05-31 08:49:55 +0100

    bump: version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -11,7 +11,7 @@ import os
 project = 'Galv'
 copyright = '2023, Oxford RSE'
 author = 'Oxford RSE'
-release = '2.1.33'
+release = '2.1.34'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
