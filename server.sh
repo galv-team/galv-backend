616b81346b598e5ddc38abe3f635b48f7fce0842
Matt Jaquiery <matt.jaquiery@dtc.ox.ac.uk>
2025-01-21 12:10:12 +0000

    fix: worker timeout

--- server.sh
+++ server.sh
@@ -5,15 +5,17 @@
 
 set -e
 
+WORKERS_PER_CORE = ${WORKERS_PER_CORE:-2}
+WORKERS = ${WORKERS:-$(nproc * WORKERS_PER_CORE)}
+TIMEOUT = ${TIMEOUT:-60}
+
 # Wrap the user's command to run prematter first
 # The prematter relies on envvars, so can't be run at build time
 
 if [ -z "$1" ]; then
     # No custom command provided, so run the default
     # Determine the number of workers from number of cores * 4
-    WORKERS=$(nproc)
-    WORKERS=$((WORKERS * 1))
-    COMMAND="gunicorn --bind localhost:8000 --workers $WORKERS config.wsgi"
+    COMMAND="gunicorn --bind localhost:8000 --workers $WORKERS --timeout $TIMEOUT config.wsgi"
     MESSAGE="Starting server: $COMMAND"
 else
     COMMAND="$@"
